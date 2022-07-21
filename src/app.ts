692ca2be342de79a8f341988133f203d05a89f7c
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-21 07:38:17 -0700

    extract starting discord bot

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,16 @@
 import 'module-alias/register'
 import 'source-map-support/register'
 
-import { Client } from 'discord.js'
-import checkTwitterContract from '@/helpers/checkTwitterContract'
-import env from '@/helpers/env'
 import runApp from '@/helpers/runApp'
 import runMongo from '@/helpers/mongo'
-import startListeners from '@/helpers/startListeners'
+import startDiscordBot from '@/helpers/startDiscordBot'
 
 void (async () => {
   await runMongo()
   console.log('Mongo connected')
   console.log('Starting discord bot...')
   await startDiscordBot()
+  console.log('Discord bot started')
   await runApp()
   console.log('App started!')
 })()
-
-async function startDiscordBot() {
-  const client = new Client({
-    intents: 1 << 9,
-  })
-  client.on('ready', async () => {
-    const channel = await client.channels.fetch(env.DISCORD_CHANNEL_ID)
-
-    if (channel && 'send' in channel) {
-      await checkTwitterContract(channel)
-      startListeners(channel)
-    }
-  })
-
-  await client.login(env.DISCORD_BOT_TOKEN)
-}
---
+++ src/helpers/startDiscordBot.ts
@@ -0,0 +1,20 @@
+import { Client } from 'discord.js'
+import checkTwitterContract from '@/helpers/checkTwitterContract'
+import env from '@/helpers/env'
+import startListeners from '@/helpers/startListeners'
+
+export default async function () {
+  const client = new Client({
+    intents: 1 << 9,
+  })
+  client.on('ready', async () => {
+    const channel = await client.channels.fetch(env.DISCORD_CHANNEL_ID)
+
+    if (channel && 'send' in channel) {
+      await checkTwitterContract(channel)
+      startListeners(channel)
+    }
+  })
+
+  await client.login(env.DISCORD_BOT_TOKEN)
+}
