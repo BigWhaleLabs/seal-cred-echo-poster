f79eec5ba00afad8af452fe639ab84908c33d142
smyzin <smyzin.work@gmail.com>
2022-07-27 18:52:40 +0300

    Add tweet details to error and Buttons to re-approve/reject tweet

--- src/helpers/sendErrorOnDiscord.ts
+++ src/helpers/sendErrorOnDiscord.ts
@@ -1,18 +1,41 @@
-import { Colors, TextChannel } from 'discord.js'
-import { EmbedBuilder } from '@discordjs/builders'
+import { ButtonStyle, Colors, TextChannel } from 'discord.js'
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  EmbedBuilder,
+} from '@discordjs/builders'
 
 export default async function (
   channel: TextChannel,
   error: unknown,
-  extraTitle?: string
+  extraTitle?: string,
+  tweetDetails?: { tweetId: number; tweetContent?: string }
 ) {
+  const { tweetId, tweetContent } = tweetDetails || {}
+  const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+    new ButtonBuilder()
+      .setCustomId(`approve-${tweetId}`)
+      .setLabel('Re-Approve')
+      .setStyle(ButtonStyle.Success),
+    new ButtonBuilder()
+      .setCustomId(`reject-${tweetId}`)
+      .setLabel('Reject')
+      .setStyle(ButtonStyle.Danger)
+  )
   const embed = new EmbedBuilder()
     .setColor(Colors.DarkRed)
     .setTitle(`Error${extraTitle ? `: ${extraTitle}` : ''}`)
-    .setDescription(`${error instanceof Error ? error.message : error}`)
+    .setDescription(
+      `${
+        error instanceof Error ? error.message : error
+      } for the tweet (id: ${tweetId})${
+        tweetContent ? `: \n\n${tweetContent}` : ''
+      }`
+    )
   try {
     await channel.send({
       embeds: [embed],
+      components: tweetDetails ? [row] : undefined,
     })
   } catch (discordError) {
     console.error(
