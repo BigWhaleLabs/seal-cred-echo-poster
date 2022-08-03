4c8621819735f3bfce169a3a41ee14faa549588c
Daniil <dpancov64@gmail.com>
2022-08-03 22:04:53 +0300

    updated ledger package

--- src/helpers/postsContracts.ts
+++ src/helpers/postsContracts.ts
@@ -1,16 +1,13 @@
-import {
-  SCERC721Posts__factory,
-  SCEmailPosts__factory,
-} from '@big-whale-labs/seal-cred-posts-contract'
+import { SCPostStorage__factory } from '@big-whaSle-labs/seal-cred-posts-contract'
 import defaultProvider from '@/helpers/provider'
 import env from '@/helpers/env'
 
-export const scErc721PostsContract = SCERC721Posts__factory.connect(
+export const scErc721PostsContract = SCPostStorage__factory.connect(
   env.SC_ERC721_POSTS_CONTRACT,
   defaultProvider
 )
 
-export const scEmailPostsContract = SCEmailPosts__factory.connect(
+export const scEmailPostsContract = SCPostStorage__factory.connect(
   env.SC_ERC721_POSTS_CONTRACT,
   defaultProvider
 )
