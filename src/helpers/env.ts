e6fa7f5ca6b410bd3758811592b2fc1cca6e63bc
Daniil <dpancov64@gmail.com>
2022-08-04 15:00:34 +0300

    use bwl.gg constants

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,5 +1,11 @@
 import * as dotenv from 'dotenv'
-import { ETH_NETWORK, ETH_RPC } from '@big-whale-labs/constants'
+import {
+  ETH_NETWORK,
+  ETH_RPC,
+  SC_EMAIL_POSTS_CONTRACT_ADDRESS,
+  SC_EXTERNAL_NFT_POSTS_CONTRACT_ADDRESS,
+  SC_NFT_POSTS_CONTRACT_ADDRESS,
+} from '@big-whale-labs/constants'
 import { cleanEnv, num, str } from 'envalid'
 import { cwd } from 'process'
 import { resolve } from 'path'
@@ -12,9 +18,15 @@ export default cleanEnv(process.env, {
   PORT: num({ default: 1337 }),
   ETH_NETWORK: str({ default: ETH_NETWORK }),
   ETH_RPC: str({ default: ETH_RPC }),
-  SC_NFT_POSTS_CONTRACT_ADDRESS: str(),
-  SC_EXTERNAL_NFT_POSTS_CONTRACT_ADDRESS: str(),
-  SC_EMAIL_POSTS_CONTRACT_ADDRESS: str(),
+  SC_NFT_POSTS_CONTRACT_ADDRESS: str({
+    default: SC_NFT_POSTS_CONTRACT_ADDRESS,
+  }),
+  SC_EXTERNAL_NFT_POSTS_CONTRACT_ADDRESS: str({
+    default: SC_EXTERNAL_NFT_POSTS_CONTRACT_ADDRESS,
+  }),
+  SC_EMAIL_POSTS_CONTRACT_ADDRESS: str({
+    default: SC_EMAIL_POSTS_CONTRACT_ADDRESS,
+  }),
   DISCORD_BOT_TOKEN: str(),
   DISCORD_CHANNEL_ID: str(),
   EMAIL_TWITTER_APP_KEY: str(),
