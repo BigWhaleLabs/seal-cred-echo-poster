91592eb3472e21bd32d14218be378fbea1683571
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-08-18 13:36:49 -0700

    update at regex

--- src/helpers/messageFilters.ts
+++ src/helpers/messageFilters.ts
@@ -3,11 +3,15 @@ import langcheck = require('langcheck')
 
 const urlRegex = createUrlRegExp()
 
+const allowedAts = ['SealCredEmail', 'SealCredGNFT', 'SealCredNFT']
+const atRegex = new RegExp(
+  `(^|[^@\\w])@(?!(${allowedAts.join('|')})\\b)(\\w{1,15})\\b`
+)
+
 // Returns `false` if text passes, an error string if it doesn't
 export default [
-  (text: string) => text.includes('@') && 'contains @',
+  (text: string) => atRegex.test(text) && 'contains @',
   (text: string) => urlRegex.test(text) && 'contains links',
-  (text: string) => text.includes('#') && 'contains #',
   (text: string) => !langcheck(text) && 'not English',
   async (text: string) => {
     const languages = (await langcheck(text)) as {
