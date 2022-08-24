02d3a08b10cab0500c752695823a49aaf16db5e7
Paul Oh <paul.oh1221@gmail.com>
2022-08-24 09:16:02 -0400

    lint error

--- src/helpers/messageFilters.ts
+++ src/helpers/messageFilters.ts
@@ -20,32 +20,31 @@ export default [
   (text: string) => atRegex.test(text) && 'contains @',
   (text: string) => urlRegex.test(text) && 'contains links',
   async (text: string) => {
+    let lngDetect: cld.DetectLanguage
     try {
-      const lngDetect = await cld.detect(text)
-      const languages = lngDetect.languages
+      lngDetect = await cld.detect(text)
+    } catch (error) {
+      return false
+    }
+    const languages = lngDetect.languages
 
-      let isEnglish = !languages?.length || languages[0].code === 'en'
-      if (!isEnglish && languages?.length) {
-        const baseConfidence = +languages[0].percent
-        for (const language of languages.slice(1)) {
-          if (+language.percent < baseConfidence) {
-            break
-          }
-          if (language.code === 'en') {
-            isEnglish = true
-            break
-          }
+    let isEnglish = !languages?.length || languages[0].code === 'en'
+    if (!isEnglish && languages?.length) {
+      const baseConfidence = +languages[0].percent
+      for (const language of languages.slice(1)) {
+        if (+language.percent < baseConfidence) {
+          break
+        }
+        if (language.code === 'en') {
+          isEnglish = true
+          break
         }
       }
-      return isEnglish
-        ? false
-        : `not English (${languages
-            .map((l: cld.Language) => `${l.name} ${l.percent}%`)
-            .join(', ')})`
-    } catch (err: any) {
-      if (err.message == 'Failed to identify language') {
-        return false
-      }
     }
+    return isEnglish
+      ? false
+      : `not English (${languages
+          .map((l: cld.Language) => `${l.name} ${l.percent}%`)
+          .join(', ')})`
   },
 ] as ((text: string) => false | string)[]
