f635353f920ac64810e9c2786a6250051f4e33d3
Paul Oh <paul.oh1221@gmail.com>
2022-09-01 13:20:28 -0400

    fixed This interaction failed error

--- src/helpers/startButtonListeners.ts
+++ src/helpers/startButtonListeners.ts
@@ -1,9 +1,8 @@
-import { ButtonInteraction, Colors } from 'discord.js'
+import { ButtonInteraction } from 'discord.js'
 import { EmbedBuilder } from '@discordjs/builders'
 import { TweetModel } from '@/models/Tweet'
 import Status from '@/models/Status'
 import getChannel from '@/helpers/getChannel'
-import logError from '@/helpers/logError'
 
 export default async function () {
   const channel = await getChannel()
@@ -12,25 +11,22 @@ export default async function () {
   })
   collector.on('collect', async (interaction: ButtonInteraction) => {
     const isApprove = interaction.customId.startsWith('a')
-    // const discordUsername = interaction.user.username
+    const discordUsername = interaction.user.username
     const components = interaction.customId.split('-')
     const contractAddress = components[1]
     const blockchainId = parseInt(components[2])
-    // const embed = new EmbedBuilder()
-    //   .setColor(interaction.message.embeds[0].color)
-    //   .setTitle(
-    //     `${
-    //       isApprove ? 'Approved' : 'Rejected'
-    //     } post #${blockchainId} by ${discordUsername}`
-    //   )
-    //   .setDescription(interaction.message.embeds[0].description)
-    // try {
-    //   await interaction.message.edit({
-    //     embeds: [embed],
-    //   })
-    // } catch (error) {
-    //   logError('wat', error)
-    // }
+    const embed = new EmbedBuilder()
+      .setColor(interaction.message.embeds[0].color)
+      .setTitle(
+        `${
+          isApprove ? 'Approved' : 'Rejected'
+        } post #${blockchainId} by ${discordUsername}`
+      )
+      .setDescription(interaction.message.embeds[0].description)
+    await interaction.message.edit({
+      embeds: [embed],
+      components: [],
+    })
     await TweetModel.updateOne(
       {
         contractAddress,
