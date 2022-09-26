5325bf11c5ef53420d5c7efc2f750aefa14f1e54
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-09-26 14:07:28 -0700

    bump dependencies and apply the hacky way to cast to v2

--- src/helpers/postToFarcaster.ts
+++ src/helpers/postToFarcaster.ts
@@ -1,5 +1,29 @@
-import { publishCast } from '@standard-crypto/farcaster-js'
+import {
+  Farcaster,
+  FarcasterGuardianContentHost,
+} from '@standard-crypto/farcaster-js'
+import { Wallet } from 'ethers'
 import env from '@/helpers/env'
+
+/**
+ * Credit: https://gist.github.com/gskril/ffaa16540c35c05a2ae20c70237bd94d
+ * MODIFIED FROM STANDARD-CRYPTO LIBRARY
+ * Signs and publishes a simple text string.
+ * The cast will be attributed to the username currently registered
+ * to the given private key's address.
+ */
+const _defaultFarcaster = new Farcaster()
+async function publishCast(privateKey: string, text: string) {
+  const contentHost = new FarcasterGuardianContentHost(privateKey)
+  const signer = new Wallet(privateKey)
+  const unsignedCast = await _defaultFarcaster.prepareCast({
+    fromUsername: 'sealcaster',
+    text,
+  })
+  const signedCast = await Farcaster.signCast(unsignedCast, signer)
+  await contentHost.publishCast(signedCast)
+  return signedCast
+}
 
 export default function (post: string) {
   return publishCast(env.FARCASTER_PRIVATE_KEY, post)
