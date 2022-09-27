509002e51e1900c9cc7110b1f8852d5c97872c65
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-09-27 10:43:06 -0700

    revert casting

--- src/helpers/postToFarcaster.ts
+++ src/helpers/postToFarcaster.ts
@@ -1,9 +1,30 @@
+import {
+  Farcaster,
+  FarcasterGuardianContentHost,
+} from '@standard-crypto/farcaster-js'
 import { Wallet } from 'ethers'
-import { publishCast } from '@standard-crypto/farcaster-js'
 import env from '@/helpers/env'
-import provider from '@/helpers/provider'
 
-const wallet = new Wallet(env.FARCASTER_PRIVATE_KEY)
+/**
+ * Credit: https://gist.github.com/gskril/ffaa16540c35c05a2ae20c70237bd94d
+ * MODIFIED FROM STANDARD-CRYPTO LIBRARY
+ * Signs and publishes a simple text string.
+ * The cast will be attributed to the username currently registered
+ * to the given private key's address.
+ */
+const _defaultFarcaster = new Farcaster()
+async function publishCast(privateKey: string, text: string) {
+  const contentHost = new FarcasterGuardianContentHost(privateKey)
+  const signer = new Wallet(privateKey)
+  const unsignedCast = await _defaultFarcaster.prepareCast({
+    fromUsername: 'sealcaster',
+    text,
+  })
+  const signedCast = await Farcaster.signCast(unsignedCast, signer)
+  await contentHost.publishCast(signedCast)
+  return signedCast
+}
+
 export default function (post: string) {
-  return publishCast(wallet, provider, post)
+  return publishCast(env.FARCASTER_PRIVATE_KEY, post)
 }
