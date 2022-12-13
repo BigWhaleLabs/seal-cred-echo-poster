36142eb1ef17e68b6c30567f8f27d77e0098f37c
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-12-13 14:46:58 -0800

    fix posting

--- src/helpers/postToFarcaster.ts
+++ src/helpers/postToFarcaster.ts
@@ -1,32 +1,11 @@
-import {
-  Farcaster,
-  FarcasterGuardianContentHost,
-} from '@standard-crypto/farcaster-js'
+import { MerkleAPIClient } from '@standard-crypto/farcaster-js'
 import { Wallet } from 'ethers'
 import env from '@/helpers/env'
 
-/**
- * Credit: https://gist.github.com/gskril/ffaa16540c35c05a2ae20c70237bd94d
- * MODIFIED FROM STANDARD-CRYPTO LIBRARY
- * Signs and publishes a simple text string.
- * The cast will be attributed to the username currently registered
- * to the given private key's address.
- */
-const _defaultFarcaster = new Farcaster()
-async function publishCast(privateKey: string, text: string, replyTo?: string) {
-  const contentHost = new FarcasterGuardianContentHost(privateKey)
-  const signer = new Wallet(privateKey)
-  const unsignedCast = await _defaultFarcaster.prepareCast({
-    fromUsername: 'sealcaster',
-    text,
-    replyTo,
-  })
-  const signedCast = await Farcaster.signCast(unsignedCast, signer)
-  await contentHost.publishCast(signedCast)
-  return signedCast
-}
+const client = new MerkleAPIClient(new Wallet(env.FARCASTER_PRIVATE_KEY))
 
 export default async function (post: string, replyToId?: string) {
-  const cast = await publishCast(env.FARCASTER_PRIVATE_KEY, post, replyToId)
-  return cast.merkleRoot
+  const cast = replyToId ? await client.fetchCast(replyToId) : undefined
+  const publishedCast = await client.publishCast(post, cast)
+  return publishedCast.hash
 }
